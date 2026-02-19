a951f42a348c992c125a2744dc5a47cb6833335e
AntonyJoySathish(BSoft) <antonyjoysathish@dbcyelagiri.edu.in>
2026-02-19 10:49:02 +0530

    program offering doctype updated #28

--- slcm/admission/doctype/program_offering/program_offering.js
+++ slcm/admission/doctype/program_offering/program_offering.js
@@ -6,14 +6,11 @@ frappe.ui.form.on("Program Offering", {
         if (!frm.doc.admission_year) {
             return;
         }
-        configure_stages(frm);
+        // configure_stages(frm);
         set_reservation_rule_query(frm, field_name = "reservation_rule");
         set_reservation_rule_query(frm, field_name = "eligibility_rule");
         set_reservation_rule_query(frm, field_name = "program_fee");
         toggle_reservation_rule(frm);
-        frm.add_custom_button("Add Programs", function () {
-            open_program_selector(frm);
-        });
     },
 
     async onload(frm) {
@@ -73,57 +70,57 @@ function toggle_reservation_rule(frm) {
     frm.refresh_field("programs");
 }
 
-function configure_stages(frm) {
-    frappe.call({
-        method: "slcm.admission.doctype.program_offering.program_offering.configuration_settings",
-        args: {
-            admission_year: frm.doc.admission_year
-        },
-        callback: function (r) {
-            if (!r.message) return;
+// function configure_stages(frm) {
+//     frappe.call({
+//         method: "slcm.admission.doctype.program_offering.program_offering.configuration_settings",
+//         args: {
+//             admission_year: frm.doc.admission_year
+//         },
+//         callback: function (r) {
+//             if (!r.message) return;
 
-            if (r.message.status === "Error") {
-                frappe.msgprint({
-                    title: "Error",
-                    message: r.message.message,
-                    indicator: "red"
-                })
-                return;
-            }
+//             if (r.message.status === "Error") {
+//                 frappe.msgprint({
+//                     title: "Error",
+//                     message: r.message.message,
+//                     indicator: "red"
+//                 })
+//                 return;
+//             }
 
-            if (!r.message.enable_interview) {
-                frm.set_value("interview_required", 0);
-            }
-            frm.set_df_property(
-                "interview_required",
-                "read_only",
-                r.message.enable_interview ? 0 : 1
-            );
+//             if (!r.message.enable_interview) {
+//                 frm.set_value("interview_required", 0);
+//             }
+//             frm.set_df_property(
+//                 "interview_required",
+//                 "read_only",
+//                 r.message.enable_interview ? 0 : 1
+//             );
 
-            if (!r.message.enable_scholarship) {
-                frm.set_value("scholarship_applicable", 0);
-            }
-            frm.set_df_property(
-                "scholarship_applicable",
-                "read_only",
-                r.message.enable_scholarship ? 0 : 1
-            );
+//             if (!r.message.enable_scholarship) {
+//                 frm.set_value("scholarship_applicable", 0);
+//             }
+//             frm.set_df_property(
+//                 "scholarship_applicable",
+//                 "read_only",
+//                 r.message.enable_scholarship ? 0 : 1
+//             );
 
-            if (!r.message.enable_reservation) {
-                frm.set_value("is_reservation_applicable", 0);
-            }
-            frm.set_df_property(
-                "is_reservation_applicable",
-                "read_only",
-                r.message.enable_reservation ? 0 : 1
-            );
+//             if (!r.message.enable_reservation) {
+//                 frm.set_value("is_reservation_applicable", 0);
+//             }
+//             frm.set_df_property(
+//                 "is_reservation_applicable",
+//                 "read_only",
+//                 r.message.enable_reservation ? 0 : 1
+//             );
 
-        },
-        error: (e) => {
-            frappe.msgprint(e.message);
-        }
-    });
-}
+//         },
+//         error: (e) => {
+//             frappe.msgprint(e.message);
+//         }
+//     });
+// }
 
 
 function child_duplicate_entry(frm, cdt, cdn, child_table, field_name, label) {
@@ -160,54 +157,3 @@ function child_duplicate_entry(frm, cdt, cdn, child_table, field_name, label) {
     }
 }
 
-function open_program_selector(frm) {
-
-    new frappe.ui.form.MultiSelectDialog({
-        doctype: "Program",
-        target: frm,
-
-        setters: {
-            program_category: frm.doc.program_category,
-            campus: frm.doc.campus
-        },
-
-        add_filters_group: 1,
-
-        get_query() {
-            return {
-                filters: {
-                    campus: frm.doc.campus,
-                    is_active: 1
-                }
-            };
-        },
-
-        action(selections) {
-
-            if (!selections.length) {
-                frappe.msgprint("Please select at least one Program");
-                return;
-            }
-
-            selections.forEach(program => {
-
-                // Prevent duplicate entry
-                let exists = frm.doc.programs.some(d => d.program === program);
-
-                if (!exists) {
-                    let row = frm.add_child("programs");
-                    row.program = program;
-                }
-
-            });
-
-            frm.refresh_field("programs");
-
-            frappe.show_alert({
-                message: "Programs Added Successfully",
-                indicator: "green"
-            });
-
-        }
-    });
-}
--- slcm/admission/doctype/program_offering/program_offering.py
+++ slcm/admission/doctype/program_offering/program_offering.py
@@ -5,24 +5,20 @@ from frappe.model.document import Document
 class ProgramOffering(Document):
 	def validate(self):
 		self.validate_availability()
+		self.sync_with_admission_year()
 
 	def validate_availability(self):
 		if self.is_available_for_admission:
-			# Cache campus and admission year
 			campus = frappe.get_doc("Campus", self.campus)
 			admission_year = frappe.get_doc("Admission Year", self.admission_year)
 
-			# 1. Campus must be active
 			if not campus.is_active:
 				frappe.throw(_("Campus {0} is not active. Cannot enable Program Offering.").format(self.campus))
 
-			# 2. Admission Year must be Open
-				
+			if not admission_year.allow_campus_enrollment:
 				frappe.throw(_("Admission Year {0} is not allowing campus enrollment. Current status: {1}").format(
 					self.admission_year, admission_year.allow_campus_enrollment
 				))
-			
-			# 3. Campus must be part of Admission Year
 			is_participating = any(c.campus == self.campus and c.is_active for c in admission_year.participating_campuses)
 			if not is_participating:
 				frappe.throw(_("Campus {0} is not a participating active campus for Admission Year {1}").format(
@@ -31,13 +27,54 @@ class ProgramOffering(Document):
 
 
 	def before_save(self):
-		# Cannot disable program if applications already submitted
 		if not self.is_available_for_admission and not self.is_new():
-			# Check if any applications exist for this program offering
-			# Assuming Application doctype name is "Student Application" or similar, 
-			# but requirements say "Application DocType"
 			if frappe.db.exists("Student Application", {"program_offering": self.name}):
 				frappe.throw(_("Cannot disable Program Offering {0} as applications have already been submitted").format(self.name))
+
+	def on_update(self):
+		self.sync_with_admission_year()
+
+
+	def sync_with_admission_year(self):
+		admission_year_name = frappe.db.get_value(
+			"Admission Year",
+			{"is_active": 1},
+			"name"
+		)
+
+		if not admission_year_name:
+			return
+
+		admission_year = frappe.get_doc("Admission Year", admission_year_name)
+
+		current_academic_year = frappe.db.get_single_value(
+			"Admission Settings",
+			"current_academic_year"
+		)
+
+		if admission_year.academic_year != current_academic_year:
+			frappe.throw(
+				_("Admission Year {0} is not the current academic year.")
+				.format(admission_year.academic_year)
+			)
+
+		#  Find existing campus row
+		for row in admission_year.participating_campuses:
+			if row.campus == self.campus:
+
+				# Update only existing row
+				row.eligibility_criteria = self.eligibility_based
+				row.entrence_test = self.need_entrence_test
+				row.schedule_interview = self.interview_required
+				row.merit_list = self.merit_list
+				row.offer_scholarship = self.scholarship_applicable
+				row.reservation_applied = self.is_reservation_applicable
+				row.is_active = self.is_available_for_admission
+
+				admission_year.save()
+				break
+
+
 
 
 @frappe.whitelist()
@@ -69,26 +106,26 @@ def configuration_settings(admission_year):
 		 }
 
 
-@frappe.whitelist()
-@frappe.validate_and_sanitize_search_inputs
-def get_filtered_reservation_rule(doctype, txt, searchfield, start, page_len, filters):
+# @frappe.whitelist()
+# @frappe.validate_and_sanitize_search_inputs
+# def get_filtered_reservation_rule(doctype, txt, searchfield, start, page_len, filters):
 
-    return frappe.db.sql("""
-        SELECT DISTINCT rr.name
-        FROM `tabProgram Offering Criteria` poc
-        INNER JOIN `tabReservation Rule` rr
-            ON rr.name = poc.reservation_rule
-        WHERE poc.program = %(program)s
-        AND poc.campus = %(campus)s
-        AND poc.admission_year = %(admission_year)s
-        AND rr.docstatus < 2
-        AND rr.name LIKE %(txt)s
-        LIMIT %(start)s, %(page_len)s
-    """, {
-        "program": filters.get("program"),
-        "campus": filters.get("campus"),
-        "admission_year": filters.get("admission_year"),
-        "txt": "%" + txt + "%",
-        "start": start,
-        "page_len": page_len
-    })
+#     return frappe.db.sql("""
+#         SELECT DISTINCT rr.name
+#         FROM `tabProgram Offering Criteria` poc
+#         INNER JOIN `tabReservation Rule` rr
+#             ON rr.name = poc.reservation_rule
+#         WHERE poc.program = %(program)s
+#         AND poc.campus = %(campus)s
+#         AND poc.admission_year = %(admission_year)s
+#         AND rr.docstatus < 2
+#         AND rr.name LIKE %(txt)s
+#         LIMIT %(start)s, %(page_len)s
+#     """, {
+#         "program": filters.get("program"),
+#         "campus": filters.get("campus"),
+#         "admission_year": filters.get("admission_year"),
+#         "txt": "%" + txt + "%",
+#         "start": start,
+#         "page_len": page_len
+#     })
