31301326b3bf08e752fe804491ffda2be0a53a19
Santhosh Kumar K <santhoshkumar@dbcyelagiri.edu.in>
2026-02-20 12:22:31 +0530

    Implement granular fee structure with a new child DocType and update offer letter generation.

--- slcm/admission/doctype/offer_configuration/offer_configuration.js
+++ slcm/admission/doctype/offer_configuration/offer_configuration.js
@@ -23,6 +23,14 @@ frappe.ui.form.on("Offer Configuration", {
                     'template_for_offer_letter': 1
                 }
             }
+        }),
+        frm.set_query('fee_structure', function () {
+            return {
+                filters: {
+                    'status': 'Active',
+                    'applicable': 'Applicant'
+                }
+            }
         })
     }
 });
