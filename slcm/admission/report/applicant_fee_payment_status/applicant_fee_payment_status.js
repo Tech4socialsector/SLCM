d12f52827ff010a84fbe1b5b2fc3b551c3f616ae
Santhosh Kumar K <santhoshkumar@dbcyelagiri.edu.in>
2026-02-28 10:42:30 +0530

    Introduce a new admission dashboard, add several new number cards, charts, and reports, and update existing number card configurations. 48

---
+++ slcm/admission/report/applicant_fee_payment_status/applicant_fee_payment_status.js
@@ -0,0 +1,39 @@
+frappe.query_reports["Applicant Fee Payment Status"] = {
+    filters: [
+        {
+            "fieldname": "academic_year",
+            "label": __("Academic Year"),
+            "fieldtype": "Link",
+            "options": "Academic Year",
+            "default": frappe.defaults.get_user_default("academic_year")
+        },
+        {
+            "fieldname": "program",
+            "label": __("Program"),
+            "fieldtype": "Link",
+            "options": "Program"
+        },
+        {
+            "fieldname": "status",
+            "label": __("Status"),
+            "fieldtype": "Select",
+            "options": "\nDraft\nAssigned\nPartially Paid\nPaid\nCancelled\nConverted"
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date"
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date"
+        },
+        {
+            "fieldname": "applicant",
+            "label": __("Applicant"),
+            "fieldtype": "Link",
+            "options": "Applicant"
+        }
+    ]
+};
