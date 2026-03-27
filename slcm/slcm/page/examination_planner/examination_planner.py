7bba5296d9bf11a0df711bdefd4771d234469133
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-03-27 07:17:35 -0400

    Fix: Examination Planner error fix

---
+++ slcm/slcm/page/examination_planner/examination_planner.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2026, CU and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+@frappe.whitelist()
+def get_components():
+	return frappe.get_all(
+		"Exam Component",
+		filters={"is_active": 1},
+		fields=["name", "component_name", "component_type"],
+		order_by="component_name asc",
+	)
+
+
+@frappe.whitelist()
+def get_assessment_types():
+	return frappe.get_all(
+		"Exam Assessment Type",
+		filters={"is_active": 1},
+		fields=["name", "type_name"],
+		order_by="type_name asc",
+	)
