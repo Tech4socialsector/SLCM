53c9bd4b0bb962b0fb58e563c4feeae2290a98a5
AntonyJoySathish(BSoft) <antonyjoysathish@dbcyelagiri.edu.in>
2026-04-09 10:44:46 +0530

    clean up applicant and admission cycle program configurations

--- slcm/admission/doctype/applicant/applicant_dashboard.py
+++ slcm/admission/doctype/applicant/applicant_dashboard.py
@@ -1,18 +1,6 @@
 def get_data():
 	return {
 		"fieldname": "applicant",
-		"non_standard_fieldnames": {
-			"Eligibility Result": "applicant_id"
-		},
-		"transactions": [
-			{
-				"label": "Communications",
-				"fieldname": "applicant",
-				"items": ["Applicant Communication Log"]
-			},
-			{
-				"label": "Admission",
-				"items": ["Admission Application", "Eligibility Result", "Offer Letter"]
-			}
-		]
+		"non_standard_fieldnames": {},
+		"transactions": []
 	}
