c4add9b091821107bb70aedcb0a57a64e7a83283
Santhosh Kumar K <santhoshkumar@dbcyelagiri.edu.in>
2026-04-09 17:09:09 +0530

    feat: implement PACE application doctypes and enhance web form functionality with file upload and validation utilities

--- slcm/pace/doctype/pace_ug_degree_details/pace_ug_degree_details.py
+++ slcm/pace/doctype/pace_ug_degree_details/pace_ug_degree_details.py
@@ -5,5 +5,9 @@
 from frappe.model.document import Document
 
 
+<<<<<<<< HEAD:slcm/pace/doctype/pace_ug_degree_details/pace_ug_degree_details.py
 class PACEUGDegreeDetails(Document):
+========
+class PACEApplication(Document):
+>>>>>>>> 1cf4b73 (feat: implement PACE application doctypes and enhance web form functionality with file upload and validation utilities):slcm/pace/doctype/pace_application/pace_application.py
 	pass
