a92c602292ce29939ba331ec3bf8a8ede78f2ae9
jenifarazim <jenifar.a@azimpremjifoundation.org>
2026-05-12 10:53:05 +0530

    Promotion Policy

--- slcm/slcm/doctype/promotion_policy/promotion_policy.py
+++ slcm/slcm/doctype/promotion_policy/promotion_policy.py
@@ -13,3 +13,7 @@ class PromotionPolicy(Document):
 			frappe.throw("Please set a Minimum CGPA value when CGPA Check is enabled.")
 		if self.enable_attendance_check and not self.min_attendance_percent:
 			frappe.throw("Please set a Minimum Attendance % when Attendance Check is enabled.")
+		if self.enable_course_shortage_check and (self.max_shortage_courses is None or self.max_shortage_courses < 0):
+			frappe.throw("Please set Max Courses with Attendance Shortage (0 or more) when Course Shortage Check is enabled.")
+		if self.enable_cf_check and (self.max_cf_fa_shortage is None or self.max_cf_fa_shortage < 0):
+			frappe.throw("Please set Max CF Courses with FA + Shortage (0 or more) when Carry-Forward Check is enabled.")
