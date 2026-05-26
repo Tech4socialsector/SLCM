94bcd05dc7d2cf71d189ac3d33ac1f39c777fbf5
AntonyJoySathish(BSoft) <antonyjoysathish@dbcyelagiri.edu.in>
2026-05-26 17:24:18 +0530

    implement custom signup email templates and improve registration password security flows

--- slcm/hooks.py
+++ slcm/hooks.py
@@ -470,8 +470,7 @@ website_route_rules = [
     {"from_route": "/pace/admission/<name>", "to_route": "pace/pace_programme_details"},
     {"from_route": "/pace/progress-tracker", "to_route": "pace_progress_tracker"},
     {"from_route": "/pace/login", "to_route": "pace/login"},
-    {"from_route": "/pace/forgot_password", "to_route": "pace/forgot_password"},
-    {"from_route": "/pace/update_password", "to_route": "pace/update_password"}
+    {"from_route": "/pace/forgot_password", "to_route": "pace/forgot_password"}
 ]
 
 update_website_context = "slcm.admission.utils.portal.update_website_context"
