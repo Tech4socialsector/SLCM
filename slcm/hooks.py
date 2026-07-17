5bcb93be147b6bb8713822b346e5f6d46ea4d4af
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-07-17 15:48:40 +0530

    Feat: "Continue with Google" login for the Student Portal

--- slcm/hooks.py
+++ slcm/hooks.py
@@ -426,6 +426,7 @@ before_request = [
     "slcm.admission.portal_application_web_form.slcm_before_request"
 ]
 on_logout = "slcm.utils.auth_routing.handle_logout"
+on_session_creation = ["slcm.utils.auth_routing.enforce_student_google_login"]
 # after_request = ["slcm.utils.after_request"]
 
 # Job Events
