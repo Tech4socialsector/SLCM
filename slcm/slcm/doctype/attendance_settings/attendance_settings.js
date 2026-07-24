43694aaa872ad798a6dd2df17fd9b446186825b0
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-07-24 17:35:04 +0530

    Fix: Condonation added in Attendance settings

--- slcm/slcm/doctype/attendance_settings/attendance_settings.js
+++ slcm/slcm/doctype/attendance_settings/attendance_settings.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2026, Nishanth and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Attendance Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("Attendance Settings", {
+	refresh(frm) {
+		frm.add_custom_button(__("Go to Attendance Condonation List"), function () {
+			frappe.set_route("List", "Student Attendance Condonation");
+		});
 
-// 	},
-// });
+		frm.set_query("authority", "level_one_authority", () => {
+			return {
+				query: "slcm.slcm.doctype.attendance_settings.attendance_settings.get_users_by_role",
+				filters: { role: "AAD" }
+			};
+		});
+
+		frm.set_query("authority", "level_two_authority", () => {
+			return {
+				query: "slcm.slcm.doctype.attendance_settings.attendance_settings.get_users_by_role",
+				filters: { role: "Programme Chair" }
+			};
+		});
+	},
+});
