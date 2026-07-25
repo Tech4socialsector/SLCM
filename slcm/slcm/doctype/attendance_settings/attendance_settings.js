4a37afe44da3e672986fed8ecae295fd50502af5
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-07-25 13:45:43 +0530

    Fix: Attendance summary report updated

--- slcm/slcm/doctype/attendance_settings/attendance_settings.js
+++ slcm/slcm/doctype/attendance_settings/attendance_settings.js
@@ -3,10 +3,6 @@
 
 frappe.ui.form.on("Attendance Settings", {
 	refresh(frm) {
-		frm.add_custom_button(__("Go to Attendance Condonation List"), function () {
-			frappe.set_route("List", "Student Attendance Condonation");
-		});
-
 		frm.set_query("authority", "level_one_authority", () => {
 			return {
 				query: "slcm.slcm.doctype.attendance_settings.attendance_settings.get_users_by_role",
@@ -20,5 +16,26 @@ frappe.ui.form.on("Attendance Settings", {
 				filters: { role: "Programme Chair" }
 			};
 		});
+
+		toggle_condonation_list_button(frm);
+	},
+
+	on_tab_change(frm) {
+		toggle_condonation_list_button(frm);
 	},
 });
+
+function toggle_condonation_list_button(frm) {
+	const label = __("Go to Attendance Condonation List");
+	const active_tab_fieldname = frm.get_active_tab()?.df?.fieldname;
+
+	if (active_tab_fieldname === "tab_condonation") {
+		if (!frm.custom_buttons[label]) {
+			frm.add_custom_button(label, () => {
+				frappe.set_route("List", "Student Attendance Condonation");
+			});
+		}
+	} else {
+		frm.remove_custom_button(label);
+	}
+}
