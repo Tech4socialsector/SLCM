84937c4a2f122660f28101da6b4d482146abed90
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-08-03 12:25:15 +0530

    Fix: Multi faculty selection with Section in the Course Offering

--- slcm/slcm/doctype/course_offering/course_offering.js
+++ slcm/slcm/doctype/course_offering/course_offering.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, Nishanth and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Course Offering", {
-// 	refresh(frm) {
+frappe.ui.form.on("Course Offering", {
+	refresh(frm) {
+		set_faculty_table_section_query(frm);
+	},
+	cohort(frm) {
+		set_faculty_table_section_query(frm);
+	},
+});
 
-// 	},
-// });
+function set_faculty_table_section_query(frm) {
+	frm.set_query("section", "faculty_table", function () {
+		return { filters: { batch: frm.doc.cohort } };
+	});
+}
